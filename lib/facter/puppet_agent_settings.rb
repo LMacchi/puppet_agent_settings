6e5a54b34659ebfbf0c9b52bd890a9c3dfe0ac7f
Laura Macchi <lau.macchi@gmail.com>
2016-09-02 13:34:07 -0500

    Create puppet_agent_settings.rb

---
+++ lib/facter/puppet_agent_settings.rb
@@ -0,0 +1,19 @@
+# Fact that displays selected configuration settings from the agent
+Facter.add(:puppet_agent_settings) do
+  setcode do
+    puppet_agent_settings = {}
+    
+    # Select all values to display
+    # Config: Path to puppet.conf
+    # Confdir: Path to configuration directory
+    # Noop: Is your agent running in noop mode?
+    keys = ['config', 'confdir', 'noop']
+    
+    keys.each do |key|
+      puppet_agent_settings[key] = Puppet.settings[key]
+    end
+    
+    # Output the populated hash
+    puppet_agent_settings
+  end
+end
