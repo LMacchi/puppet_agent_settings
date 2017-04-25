4608b0b633be8d40d3ec39eb5536a52b20f7ef61
LMacchi <lau.macchi@gmail.com>
2017-04-24 19:22:48 -0500

    Add spec tests

---
+++ spec/unit/facter/puppet_agent_settings_spec.rb
@@ -0,0 +1,19 @@
+# Test that the keys collected exist inside the settings hash
+require "spec_helper"
+
+describe Facter::Util::Fact do
+  before {
+    Facter.clear
+  }
+
+  settings = ['confdir','config','ssldir']
+
+  describe "puppet_agent_settings" do
+    settings.each do |s|
+      it "key #{s} should not be nil" do
+        expect(Facter.fact(:puppet_agent_settings).value[s]).to be_truthy
+      end
+    end
+  end
+
+end
