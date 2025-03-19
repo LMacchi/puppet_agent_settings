f632555ae667cf88abb9709d1bdcebb6acd449c7
Laura Macchi <lmacchi@splunk.com>
2025-03-19 16:20:45 -0600

    Add new settings for certificates

--- lib/facter/puppet_agent_settings.rb
+++ lib/facter/puppet_agent_settings.rb
@@ -1,4 +1,5 @@
 # Fact that displays selected configuration settings from the agent
+# Omit the ones already exposed by Puppet - https://www.puppet.com/docs/puppet/latest/lang_facts_builtin_variables.html#lang_facts_builtin_variables-agent-facts
 Facter.add(:puppet_agent_settings) do
   setcode do
     puppet_agent_settings = {}
@@ -7,7 +8,11 @@ Facter.add(:puppet_agent_settings) do
     # Config: Path to puppet.conf
     # Confdir: Path to configuration directory
     # SSLDir: Path to your SSL directory
-    keys = ['config', 'confdir', 'ssldir']
+    # HostPrivKey: Path to the host SSL Private Key
+    # HostPubKey: Path to the host SSL Public Key
+    # LocalCACert: Path to the host local copy of the CA certificate
+    # HostCert: Path to the host local SSL certificate file
+    keys = ['config', 'confdir', 'ssldir', 'hostprivkey', 'hostpubkey', 'localcacert', 'hostcert']
 
     keys.each do |key|
       puppet_agent_settings[key] = Puppet.settings[key]
